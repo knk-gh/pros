1584b6f428ddc15e94f2090322db081ca481ff26
knk-gh <knk0pro@gmail.com>
2018-11-27 15:14:14 +0900

    [add]1127-final

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name: "管理人",
-profile: "管理人です。よろしくね",
-twitter_url: "1234567",
-pixiv_url: "1234567",
+User.create!(name: "かりびと",
+profile: "かりびとです。よろしくね",
+twitter_url: "http://1234567",
+pixiv_url: "http://1234567",
 status: "1",
-email: "a@a",
-password: "aaaaaa",
-password_confirmation: "aaaaaa",
+report: "焼肉を求めている",
+email: "az@za",
+password: "aaa1130",
+password_confirmation: "aaa1130",
 admin: true)
